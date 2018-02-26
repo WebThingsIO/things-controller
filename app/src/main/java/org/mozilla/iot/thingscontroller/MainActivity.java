1dd54e673ad35e8e914b40f9f55fa1bd154c0265
Ben Francis <ben@tola.me.uk>
2018-02-26 20:04:53 +0000

    Add basic system chrome

--- app/src/main/java/org/mozilla/iot/thingscontroller/MainActivity.java
+++ app/src/main/java/org/mozilla/iot/thingscontroller/MainActivity.java
@@ -2,31 +2,73 @@ package org.mozilla.iot.thingscontroller;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import java.io.IOException;
 
 /**
- * Skeleton of an Android Things activity.
- * <p>
- * Android Things peripheral APIs are accessible through the class
- * PeripheralManagerService. For example, the snippet below will open a GPIO pin and
- * set it to HIGH:
- * <p>
- * <pre>{@code
- * PeripheralManagerService service = new PeripheralManagerService();
- * mLedGpio = service.openGpio("BCM6");
- * mLedGpio.setDirection(Gpio.DIRECTION_OUT_INITIALLY_LOW);
- * mLedGpio.setValue(true);
- * }</pre>
- * <p>
- * For more complex peripherals, look for an existing user-space driver, or implement one if none
- * is available.
- *
- * @see <a href="https://github.com/androidthings/contrib-drivers#readme">https://github.com/androidthings/contrib-drivers#readme</a>
+ * Things Controller.
  */
 public class MainActivity extends Activity {
+
+    private static final String HOME_PAGE = "https://mygatewayname.mozilla-iot.org";
+
+    private View mContentView;
+    private WebView webview;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mContentView = findViewById(R.id.main_content);
+        webview = findViewById(R.id.webview);
+        webview.setWebViewClient(new WebViewClient());
+        webview.getSettings().setJavaScriptEnabled(true);
+        webview.getSettings().setDomStorageEnabled(true);
+        webview.setInitialScale(100);
+        webview.loadUrl(HOME_PAGE);
+    }
+
+    /**
+     * Navigate to the home page.
+     *
+     * @param view
+     */
+    public void home(View view) {
+        webview.loadUrl(HOME_PAGE);
+    }
+
+    /**
+     * Navigate back in session history.
+     *
+     * @param view
+     */
+    public void back(View view) {
+        webview.goBack();
+    }
+
+    /**
+     * Reload the current page.
+     *
+     * @param view
+     */
+    public void reload(View view) {
+        webview.reload();
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if (hasFocus) {
+            mContentView.setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);}
     }
 }

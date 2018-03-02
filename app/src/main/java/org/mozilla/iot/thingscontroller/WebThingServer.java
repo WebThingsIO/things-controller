0f6e6bae17b42a64719d75a0c1b84025e622aa8c
Ben Francis <ben@tola.me.uk>
2018-03-02 20:08:13 +0000

    Serve a basic Thing Description

--- app/src/main/java/org/mozilla/iot/thingscontroller/WebThingServer.java
+++ app/src/main/java/org/mozilla/iot/thingscontroller/WebThingServer.java
@@ -1,7 +1,9 @@
 package org.mozilla.iot.thingscontroller;
 
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
 
+import fi.iki.elonen.NanoHTTPD;
 import fi.iki.elonen.router.RouterNanoHTTPD;
 
 /**
@@ -19,8 +21,37 @@ public class WebThingServer extends RouterNanoHTTPD {
         System.out.println("Web Thing Server running on http://localhost:" + PORT + "/ \n");
     }
 
+    public static class ThingDescriptionHandler extends DefaultHandler {
+      @Override
+      public String getText() {
+          String text = "{"
+            + "\"name\": \"Things Controller\","
+            + "\"type\": \"thing\","
+            + "\"description\": \"A Web of Things controller from Mozilla\""
+            + "}";
+          return text;
+      }
+
+      public String getMimeType() {
+          return "application/json";
+      }
+
+      @Override
+      public NanoHTTPD.Response.IStatus getStatus() {
+          return NanoHTTPD.Response.Status.OK;
+      }
+
+      public NanoHTTPD.Response get(UriResource uriResource) {
+          String text = getText();
+          ByteArrayInputStream input = new ByteArrayInputStream(text.getBytes());
+          int size = text.getBytes().length;
+          return NanoHTTPD.newFixedLengthResponse(getStatus(), getMimeType(), input, size);
+      }
+    }
+
     @Override
     public void addMappings() {
         super.addMappings();
+        addRoute("/", ThingDescriptionHandler.class);
     }
 }
